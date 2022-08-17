9701e0711415d4d2c30b061d7690cd2aa2b50bd2
AurelianoAlencar <alencarsobreira@terra.com.br>
2022-08-16 21:34:06 -0300

    Projetos criados

---
+++ backend/src/test/java/com/dvsuperior/dwvendas/DwvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvsuperior.dwvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
